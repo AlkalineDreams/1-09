63182a7b4b768f946285f6e15ad4ad239f37e1da
jessgriffin <jessica.griffin@ocsbstudent.ca>
2016-09-22 12:44:33 -0400

    Update MARK.java

--- MARK.java
+++ MARK.java
@@ -1,17 +1,16 @@
 /****************************************************************************
  *
- * Created by: Jess and Craig
+ * Created by: Jess and Craig, with some fantastic help from michael
  * Created on: Sept 2016
  * This program fills an array with 10 numbers and returns the max and min
  *  values
  ****************************************************************************/
-package markifier;
+
 
 public class MARK {
 
-	public static void main(String[] args) {
-		// Creating Array
-		int[][] markArray = new int[4][6];
+	public static int ClassAverage (int [][] markArray){
+		//fills up the array with random numbers
 		int total = 0;
 		
 		for (int x = 0; x < markArray.length; x++){
@@ -23,8 +22,17 @@ public class MARK {
 				//System.out.println("total: " + total);
 			}
 		}
-		
 		int average = total / (markArray.length * markArray[0].length);
+		return average;
+	}
+	
+	public static void main(String[] args) {
+		// Creating Array
+		int[][] markArray = new int[4][6];
+		
+		int average = ClassAverage(markArray);
+		//finding average
+		
 		System.out.println("The average is: " + average);
 		
 	}
